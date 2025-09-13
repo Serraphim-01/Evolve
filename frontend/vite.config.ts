df2e846dbc851a7b42424a2607a5d62fb9916712
Serraphim-01 <ojeremiah721@gmail.com>
2025-09-13 15:24:26 +0100

    feat: docker integration with frontend, backend and db

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // allows access from container → localhost:3000
+    port: 3000
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
